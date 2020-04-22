30ae70d7ded586d037fb6102c8c840611819a2e5
squidtempura <d17129502@mytudublin.ie>
2020-04-22 14:02:20 +0100

    ArrayList,loadTasks,printTasks

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,23 +1,39 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {	
+	//declare an ArrayList to hold instances of the Task class
+	public ArrayList<Task> tasks = new ArrayList<Task>();
 
 	public void settings()
 	{
 		size(800, 600);
 	}
 
+	//populates the ArrayList from the file tasks.csv
 	public void loadTasks()
 	{
-		
+		Table T = loadTable("tasks.csv", "header");
+		for(TableRow row:T.rows())
+		{
+			Task t = new Task(row);
+			tasks.add(t);
+		}
 	}
 
+	//prints the elements of the ArrayList
 	public void printTasks()
 	{
-		
+		for(Task t:tasks)
+        {
+            System.out.println(t);
+        }	
 	}
 
 	public void mousePressed()
@@ -34,6 +50,8 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
